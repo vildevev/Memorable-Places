32dffaa90a5b5dd8cfea323ed57db1fa5c1b4b6f
“Vilde <vilde@vevatne.no>
2017-08-02 13:21:21 -0700

    Add logic to add pins to locations

--- ViewController.swift
+++ ViewController.swift
@@ -25,7 +25,11 @@ class ViewController: UIViewController, MKMapViewDelegate {
                                     let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
                                     let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
                                     let region = MKCoordinateRegion(center: coordinate, span: span)
-                                    self.map.setRegion(region, animated: true) 
+                                    self.map.setRegion(region, animated: true)
+                                    let annotation = MKPointAnnotation()
+                                    annotation.coordinate = coordinate
+                                    annotation.title = name
+                                    self.map.addAnnotation(annotation) 
                                 }
                             }
                         }
